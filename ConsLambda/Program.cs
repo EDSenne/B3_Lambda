d3320e023ca218f49d86d8957eb80744675285de
wilschok <william.schokkele@howest.be>
2017-02-15 15:43:37 +0100

    Statement lambda - no parameters

--- ConsLambda/Program.cs
+++ ConsLambda/Program.cs
@@ -8,10 +8,16 @@ namespace ConsLambda
 {
     class Program
     {
+        delegate double noparams();
         static void Main(string[] args)
         {
-            
-
+            noparams randomiser = () =>
+            {
+                int random = new Random().Next();
+                Console.WriteLine($"Random number generated - {random}");
+                return random;
+            };
+            randomiser();
             Console.ReadKey();
         }
     }
