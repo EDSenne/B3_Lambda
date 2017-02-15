dc3609cc3d2bd7424f6bc4ba327d20cf8e9f20f8
wilschok <william.schokkele@howest.be>
2017-02-15 15:20:46 +0100

    delegate ConvertUnit

--- ConsLambda/Program.cs
+++ ConsLambda/Program.cs
@@ -8,9 +8,19 @@ namespace ConsLambda
 {
     class Program
     {
+        public delegate double ConvertUnit(double value);
+
+        public double CalToKj(double cal)
+        {
+            return cal * 4.814;
+        }
         static void Main(string[] args)
         {
-            
+            Program p = new Program();
+            ConvertUnit cu = p.CalToKj;
+            double calorie = 20;
+            double kJ = cu(calorie);
+            Console.WriteLine($"{calorie} cal = {Math.Round(kJ, 3)} kJ");
 
             Console.ReadKey();
         }
