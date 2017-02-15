c1db45600e34531548ea03ad0e224d058307489f
wilschok <william.schokkele@howest.be>
2017-02-15 16:18:44 +0100

    Predicate in FindIndex

--- ConsLambda/Program.cs
+++ ConsLambda/Program.cs
@@ -10,8 +10,9 @@ namespace ConsLambda
     {
         static void Main(string[] args)
         {
-            
-
+            List<int> integers = new List<int> { 12, 16, 17, 28 };
+            int oddLocation = integers.FindIndex(i => i % 2 != 0);
+            Console.WriteLine($"Odd number found on position {oddLocation}");   
             Console.ReadKey();
         }
     }
